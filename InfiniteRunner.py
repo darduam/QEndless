4f9fada24ff56d59a95628508034541f1e949e38
darduam <116617934+darduam@users.noreply.github.com>
2022-10-25 18:39:37 -0300

    Add files via upload

--- InfiniteRunner.py
+++ InfiniteRunner.py
@@ -11,16 +11,22 @@ green=(0, 255, 0)
 purple=(128, 0, 128)
 red=(255, 0, 0)
 gris=(128, 128, 128)
+orange=(255, 165, 0)
+yellow=(255, 255, 0)
 
 WIDTH=450
 HEIGHT=300
 
 #game variables
 score=0
+dscore=0
 player_x=115
 player_y=250
 player2_x=115
 player2_y=250
+obstacles=[0,-100, -250]
+obstacle_speed=2
+active=True
 
 screen=pygame.display.set_mode([WIDTH,HEIGHT])
 pygame.display.set_caption('Infinite QRunner')
@@ -39,6 +45,11 @@ while running:
     floor4=pygame.draw.rect(screen,red,[300,0,70,300])
     player=pygame.draw.rect(screen,black,[player_x, player_y, 20, 20])
     player2=pygame.draw.rect(screen,gris,[player2_x, player2_y, 20, 20])
+    obstacle1=pygame.draw.rect(screen, orange,[120, obstacles[0], 10,10])
+    obstacle2=pygame.draw.rect(screen, orange,[190, obstacles[0], 10,10])
+    obstacle3=pygame.draw.rect(screen, orange,[260, obstacles[0], 10,10])
+    obstacle4=pygame.draw.rect(screen, orange,[330, obstacles[0], 10,10])
+    
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -76,6 +87,18 @@ while running:
             if event.key == pygame.K_n:
                 player_x=115
                 player2_x=325
-                            
+                
+        for i in range(len(obstacles)):
+            if active:
+                obstacles[i] += obstacle_speed
+                if obstacles[i] > 300:
+                    obstacles[i] = random.randint(-70, -170)
+                    dscore +=1
+                
+                if player.colliderect(obstacle1) or player.colliderect(obstacle2) or player.colliderect(obstacle3) or player.colliderect(obstacle4):
+                    score += 1
+                
+                    
+                    
     pygame.display.flip()
 pygame.quit()
